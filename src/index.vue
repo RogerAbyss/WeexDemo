352f3b8543cf283776120d9a11f4ca67c52e18f5
abyss <roger_ren@qq.com>
2018-01-26 11:45:06 +0800

    :tada: Init
    初始化项目

---
+++ src/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="wrapper">
+    <image :src="logo" class="logo"></image>
+    <text class="greeting">Abyss</text>
+    <text class="message">现在开始学习Weex吧!</text>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        logo: 'https://gitee.com/rogerabyss/Assert/raw/master/CI/cat.png',
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .wrapper {
+    justify-content: center;
+    align-items: center;
+  }
+  .logo {
+    width: 400px;
+    height: 400px;
+  }
+  .greeting {
+    margin-top: 70px;
+    font-size: 50px;
+    color: #41B883;
+  }
+  .message {
+    margin: 30px;
+    font-size: 32px;
+    color: #727272;
+  }
+</style>
